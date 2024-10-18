1bdf8f9c7c3f27fb543a9e71af49fa176e1ccf53
Rüzgar <140716294+WindBorn12@users.noreply.github.com>
2024-10-18 22:41:50 +0300

    Create deneme.py

---
+++ deneme.py
@@ -0,0 +1,8 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+
+word = input("anlamadiginiz kelimeyi yaziniz")
+
+print(meme_dict[word])
